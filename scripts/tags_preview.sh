23da4757765957ce0b04303a367e026f403011fb
locnguyenvu <ngynvuloc@gmail.com>
2021-06-09 12:15:08 +0700

    Update script preview code

--- scripts/tags_preview.sh
+++ scripts/tags_preview.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 INPUT="$@"
 
-declare -a FOO
-FOO=( 'a' )
-
-FILEPATH=$(echo "$@" | grep -oE '[a-zA-Z0-9]+(\-|_)*(/[a-zA-Z0-9]+.*)*\.[a-zA-z0-9]{1,5}'+"${FILETYPES}")
-echo ""
-
+FILEPATH=$(echo "$@" | grep -oE '[a-zA-Z0-9]+(\-|_)*(/[a-zA-Z0-9]+.*)*\.[a-zA-z0-9]{1,5}')
 DEFINEPATTERN=$(echo "$@" | grep -oE '\/\^.*\$\/\;' | grep -oE '[^\/\^[:space:]].*\$' | sed 's/\$$//g')
+
 LINE=$(grep -nF "${DEFINEPATTERN}" "${FILEPATH}" | cut -d: -f 1)
 LINERANGE="$((LINE-2))"
 bat -f $FILEPATH -H $LINE -r "$LINERANGE:" --style=plain
